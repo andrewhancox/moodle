428964d4578af46a5c645205bba7014787f9f6d4
Andrew Hancox <andrewdchancox@googlemail.com>
2017-07-31 11:09:08 +0100

    MDL-57455 mod_data: Import and export tags

--- mod/data/lib.php
+++ mod/data/lib.php
@@ -3101,7 +3101,7 @@ function data_export_ods($export, $dataname, $count) {
  * @return array
  */
 function data_get_exportdata($dataid, $fields, $selectedfields, $currentgroup=0, $context=null,
-                             $userdetails=false, $time=false, $approval=false) {
+                             $userdetails=false, $time=false, $approval=false, $tags = false) {
     global $DB;
 
     if (is_null($context)) {
@@ -3132,6 +3132,9 @@ function data_get_exportdata($dataid, $fields, $selectedfields, $currentgroup=0,
     }
     if ($approval) {
         $exportdata[0][] = get_string('approved', 'data');
+    }
+    if ($tags) {
+        $exportdata[0][] = get_string('tags', 'data');
     }
 
     $datarecords = $DB->get_records('data_records', array('dataid'=>$dataid));
@@ -3167,6 +3170,10 @@ function data_get_exportdata($dataid, $fields, $selectedfields, $currentgroup=0,
             }
             if ($approval) { // Add approval status
                 $exportdata[$line][] = (int) $record->approved;
+            }
+            if ($tags) {
+                $itemtags = \core_tag_tag::get_item_tags_array('mod_data', 'data_records', $record->id);
+                $exportdata[$line][] = implode(', ', $itemtags);
             }
         }
         $line++;
